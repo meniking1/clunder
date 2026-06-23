589904d359432ac306881eb710035eabe71e1eca
Men <055godman@gmail.com>
2026-06-23 14:30:08 +0300

    i add luach hakefel

---
+++ operatios.py
@@ -0,0 +1,16 @@
+from unittest import result
+
+num1= int(input("choose number"))
+
+num2= int(input("coose one more number"))
+which_one = input("choose which one(+,-,*,/)")
+if which_one == "*":
+    print (num1*num2)
+elif which_one == "+":
+    print(num1+num2)
+elif which_one == '/':
+    print(num1/num2)
+elif which_one== '-':
+    print(num1-num2)
+else:
+    print("sorry i cant understand you")
